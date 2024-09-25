30edea56cb15dfbbcd963b5af51b865fe29ed4fd
Matthew Leboffe <matthew8853@gmail.com>
2024-09-24 22:12:09 -0400

    minor changes to .gitignore and print statements

--- client1.py
+++ client1.py
@@ -52,7 +52,7 @@ class Client:
 def main():
     SERVER_IP = os.getenv('SERVER_IP')
     SERVER_PORT = 12345
-    CLIENT_ID = 'C1'  # Hardcoded client ID for now
+    CLIENT_ID = '1'  # Hardcoded client ID for now
 
     client = Client(SERVER_IP, SERVER_PORT, CLIENT_ID)
 
